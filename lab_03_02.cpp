0736353685fc8c3e8cbe98f289f5e8560aa4be15
prateekmurmu <42237322+prateekmurmu@users.noreply.github.com>
2018-08-17 11:29:33 +0530

    Add files via upload

---
+++ lab_03_02.cpp
@@ -0,0 +1,39 @@
+// incliding library
+
+#include <iostream>
+
+// using standard namespace
+
+using namespace std;
+
+// main function starts
+
+int main(){
+
+// declaration of variables
+
+		int a = 2;
+
+		char b = '3';
+
+		float c = 3.006;
+
+		double d = 4.012;
+
+		bool e = 1;
+
+// print result
+		
+		cout << "size of int - " << sizeof(int) << "byte" << ", content of a =" << a << endl;
+
+		cout << "size of char - " << sizeof(char) << "byte" << ", content of b =" << b << endl;
+
+		cout << "size of float - " << sizeof(float) << "byte" << ", content of c =" << c << endl;
+
+		cout << "size of double - " << sizeof(double) << "byte" << ", content of d =" << d << endl;
+
+		cout << "size of bool - " << sizeof(bool) << "byte" << ", content of e =" << e << endl;
+
+
+		return 0;
+}
