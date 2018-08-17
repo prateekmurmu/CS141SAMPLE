4e9908d9008a9be025d845cf0346f98ec5972f45
prateekmurmu <42237322+prateekmurmu@users.noreply.github.com>
2018-08-17 11:57:37 +0530

    Add files via upload

---
+++ lab_03_03.cpp
@@ -0,0 +1,29 @@
+// including library
+#include <iostream>
+
+// using standard namespace
+using namespace std;
+
+// main function starts
+int main(){
+		int a = 9;
+
+		int b = 1;
+
+		int c = 2;
+
+		int d = 3;
+
+		int diff = 9-1;
+
+		cout << "Difference between 9 and 1 is " << diff << endl;
+
+		int quo = diff/c;
+
+		cout << "quotient of 9-1 and 2 is " << quo << endl;
+
+		int mod = quo%d;
+
+		cout << "modulo of (9-1)/2 and 3 is " << mod << endl;
+
+		return 0; }
