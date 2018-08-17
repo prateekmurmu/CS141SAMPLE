1cada2775f17c0d5ba853ca92831f44850719cf3
prateekmurmu <42237322+prateekmurmu@users.noreply.github.com>
2018-08-17 11:33:15 +0530

    Update lab_03_01.cpp

--- lab_03_01.cpp
+++ lab_03_01.cpp
@@ -1,16 +1,21 @@
+// including library
 #include <iostream>
 
+// using standard namespace
 using namespace std;
 
+// main function starts
 int main(){
 		int a = 2;
 
 		int b = 3;
 
 		int c = a + b;
+// print 'c'	
+		cout << c;
 
 		int d = (a + b) * a;
-		
+// print 'd'		
 		cout << d;
 
 
